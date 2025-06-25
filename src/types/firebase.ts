f63c39b7cbdfc74091af5c2406d5516c8bd87d1f
Justin Emery <justinemery@gmail.com>
2025-06-25 18:11:37 +0100

    Add additional Firestore constants

--- src/types/firebase.ts
+++ src/types/firebase.ts
@@ -1,22 +1,56 @@
 export enum FirestoreCollection {
+  ACTIVITY_LOGS = 'activity-logs',
   CATEGORIES = 'categories',
   CONTRIBUTOR_STRINGS = 'strings', // sub-collection of contributors
   CONTRIBUTORS = 'contributors',
   DOCS = 'docs',
   DYNAMIC_PANELS = 'dynamicPanels',
+  EXTRAS = 'extras',
+  FILES = 'files',
   FOLDERS = 'folders',
-  METADATA = 'metadata',
+  FOLDER_THUMBS = 'folder-thumbs',
+  NEWS = 'news',
+  METADATA = 'metadata', // sub-collection of docs
   QUOTES = 'quotes',
+  REMOVALS = 'removals',
+  RETREATS = 'retreats',
   ROLES = 'roles',
   SCHEDULED_QUOTES = 'scheduledQuotes',
-  SUBTITLES = 'subtitles',
+  SUBTITLES = 'subtitles', // sub-collection of docs
   TOPICS = 'topics',
   TOPICS_POTENTIAL = 'topics-potential',
   TRANSIENTS = 'transients',
+  UPDATES = 'updates',
+  USERS = 'users',
+  USER_ACTIVITIES = 'activities', // sub-collection of users
+  USER_FAVORITES = 'favorites', // sub-collection of users
+  USER_PLAYLISTS = 'playlists', // sub-collection of users
+  USER_SETTINGS = 'settings', // sub-collection of users
+  VIDEOS = 'videos',
 }
 
 export const firestoreCollectionDocs = {
   dynamicPanels: {
     tabs: 'tabs',
+  },
+  folderThumbs: {
+    folderThumbs: 'folder-thumbs',
+  },
+  removals: {
+    removals: 'removals',
+  },
+  settings: {
+    settings: 'settings',
+  },
+  updates: {
+    updates: 'updates',
+  },
+  users: {
+    favorites: {
+      favorites: 'favorites',
+    },
+    settings: {
+      app: 'app',
+    }
   }
 };
